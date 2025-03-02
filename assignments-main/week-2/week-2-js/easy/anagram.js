0653f96b930b78db13e3f110856ad9ba0f6d85c0
Vedant3008 <shahh7107@gmail.com>
2025-03-02 09:41:38 +0530

    changes

--- assignments-main/week-2/week-2-js/easy/anagram.js
+++ assignments-main/week-2/week-2-js/easy/anagram.js
@@ -1,17 +1,17 @@
 /*
-  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+  Write a function `isAnagram` which takes 2 parameters and returns true/false if 
+  those are anagrams or not.
   What's Anagram?
-  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
+  - A word, phrase, or name formed by rearranging the letters of another, 
+  such as spar, formed from rasp.
 */
 
 function isAnagram(str1, str2) {
   for(let i of str1){
-    if (str1[i]===str2){
-      
+      console.log("isAnagram")
     }
-
-  }
-  return true;
+  return 0;
 }
 
 module.exports = isAnagram; 
+isAnagram("vedant","avednt");

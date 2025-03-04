30a813299f2e8fc4ca32488a89f2909b2b4b6bd4
Vedant3008 <shahh7107@gmail.com>
2025-03-04 08:55:14 +0530

    Completed

--- assignments-main/week-2/week-2-js/easy/anagram.js
+++ assignments-main/week-2/week-2-js/easy/anagram.js
@@ -7,11 +7,22 @@
 */
 
 function isAnagram(str1, str2) {
-  for(let i of str1){
-      console.log("isAnagram")
-    }
-  return 0;
-}
+  const lowerCaseStr1 = str1.toLowerCase();//OR const sortedStr1 = str1.toLowerCase().split("").sort().join("");
+  const arr1 = lowerCaseStr1.split("");
+  arr1.sort();
+  const arr1Sorted = arr1.join("");
 
-module.exports = isAnagram; 
-isAnagram("vedant","avednt");
+  const lowerCaseStr2 = str2.toLowerCase();//OR const sortedStr2 = str2.toLowerCase().split("").sort().join("");
+  const arr2 = lowerCaseStr2.split("");
+  arr2.sort();
+  const arr2Sorted = arr2.join("");
+
+  if (arr1Sorted === arr2Sorted){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+console.log(isAnagram("Vedant","avednt"));
+//module.exports = isAnagram; 
